bd0aebc9500b2e8e81349642ca3b7d0fc9a8c716
nahhuaraujo <nahhuaraujo@gmail.com>
2022-11-29 19:26:56 -0300

    Add conditional to show navlink only to the right roles

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,16 @@
 import { LogoutButton } from '..';
 import * as S from './Navbar.styled';
+import { AppStore } from '../../redux/store';
+import { useSelector } from 'react-redux';
+import { Roles } from '../../models';
 
 const Navbar = () => {
+  const role = useSelector((store: AppStore) => store.user.role);
   return (
     <S.Navbar>
       <S.Navlink to='home'>Home</S.Navlink>
-      <S.Navlink to='dashboard'>Dashboard</S.Navlink>
-      <S.Navlink to='about'>About</S.Navlink>
+      {role === Roles.ADMIN ? <S.Navlink to='dashboard'>Dashboard</S.Navlink> : null}
+      <S.Navlink to='/about'>About</S.Navlink>
       <LogoutButton />
     </S.Navbar>
   );
