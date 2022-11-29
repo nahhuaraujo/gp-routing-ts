696c5783c9fc2050298cbbb38a3b7845ea730699
nahhuaraujo <nahhuaraujo@gmail.com>
2022-11-29 19:25:50 -0300

    Fix logout not redirecting to login page

--- src/components/LogoutButton/LogoutButton.tsx
+++ src/components/LogoutButton/LogoutButton.tsx
@@ -1,11 +1,14 @@
+import { useNavigate } from 'react-router-dom';
 import { useUserActions } from '../../hooks';
 import * as S from './LogoutButton.styled';
 
 const Logout = () => {
   const { resetUser } = useUserActions();
+  const navigate = useNavigate();
 
   const logout = () => {
     resetUser();
+    navigate('/');
   };
 
   return <S.Logout onClick={logout}>Logout</S.Logout>;
