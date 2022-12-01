ea5c1c36a171c8a55cb58a31c419a7ddc5fc2b39
nahhuaraujo <nahhuaraujo@gmail.com>
2022-12-01 12:47:28 -0300

    Fix removed lazy loading, since was causing bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { lazy, Suspense } from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
 import * as S from './App.styled';
-import { Loading } from './components';
+import { Navbar } from './components';
 import { AuthGuard } from './guard';
+import { About, Home, Login, NotFound, Private } from './pages';
 import store from './redux/store';
 import { PrivateRoutes, PublicRoutes } from './routes';
-
-const Login = lazy(() => import('./pages/Login/Login'));
-const About = lazy(() => import('./pages/About/About'));
-const Private = lazy(() => import('./pages/Private/Private'));
-const NotFound = lazy(() => import('./pages/NotFound/NotFound'));
-
 const App = () => {
   return (
     <S.App>
-      <Suspense fallback={<Loading />}>
-        <Provider store={store}>
-          <Router>
-            <Routes>
-              <Route path='/' element={<Navigate to={PrivateRoutes.PRIVATE} />} />
-              <Route path={PublicRoutes.LOGIN} element={<Login />} />
-              <Route element={<AuthGuard />}>
-                <Route path={`${PrivateRoutes.PRIVATE}/*`} element={<Private />} />
-              </Route>
-              <Route path={PublicRoutes.ABOUT} element={<About />} />
-              <Route path='*' element={<NotFound />} />
-            </Routes>
-          </Router>
-        </Provider>
-      </Suspense>
+      <Provider store={store}>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Navigate to={PrivateRoutes.PRIVATE} />} />
+            <Route path={PublicRoutes.LOGIN} element={<Login />} />
+            <Route path={PublicRoutes.HOME} element={<Home />} />
+            <Route element={<AuthGuard />}>
+              <Route path={`${PrivateRoutes.PRIVATE}/*`} element={<Private />} />
+            </Route>
+            <Route path={PublicRoutes.ABOUT} element={<About />} />
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </Router>
+      </Provider>
     </S.App>
   );
 };
